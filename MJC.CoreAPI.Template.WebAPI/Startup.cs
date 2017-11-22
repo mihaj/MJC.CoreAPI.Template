198a6337a4cf2f053b421c1dc293e181df01349f
Miha Jakovac <miha@mjc.si>
2017-11-22 13:33:59 +0100

    small cleanup

--- MJC.CoreAPI.Template.WebAPI/Startup.cs
+++ MJC.CoreAPI.Template.WebAPI/Startup.cs
@@ -26,7 +26,6 @@ namespace MJC.CoreAPI.Template.WebAPI
             _env = env;
         }
 
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
